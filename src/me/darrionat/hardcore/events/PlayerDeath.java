fd66fa225498e36db2a3eef35bb045afbaf7b17c
Restitutor <Restitutor@users.noreply.github.com>
2021-12-18 12:06:29 -0500

    Update PlayerDeath.java

--- src/me/darrionat/hardcore/events/PlayerDeath.java
+++ src/me/darrionat/hardcore/events/PlayerDeath.java
@@ -1,16 +1,17 @@
 package me.darrionat.hardcore.events;
 
+import org.bukkit.BanList;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+
+import java.util.Date;
 
 import me.darrionat.hardcore.Hardcore;
 import me.darrionat.hardcore.services.DeathWorldService;
 import me.darrionat.hardcore.services.MessageService;
 import me.darrionat.hardcore.services.PlayerStatusService;
-import me.darrionat.hardcore.utils.Utils;
 
 public class PlayerDeath implements Listener {
 
@@ -18,29 +19,24 @@ public class PlayerDeath implements Listener {
 	private MessageService messageService;
 	private PlayerStatusService playerStatusService;
 	private DeathWorldService deathWorldService;
+	private final int hours;
 
 	public PlayerDeath(Hardcore plugin, MessageService messageService, PlayerStatusService playerStatusService,
-			DeathWorldService deathWorldService) {
+			DeathWorldService deathWorldService, int hours) {
 		this.plugin = plugin;
 		this.messageService = messageService;
 		this.playerStatusService = playerStatusService;
 		this.deathWorldService = deathWorldService;
+		this.hours = hours;
 
 		Bukkit.getPluginManager().registerEvents(this, plugin);
 	}
 
 	@EventHandler
 	public void onDeath(PlayerDeathEvent e) {
-		Player p = e.getEntity();
-		if (!playerStatusService.playerIsAlive(p)) {
-			return;
-		}
-
-		playerStatusService.setPlayerToDead(p);
-		p.kickPlayer(Utils.chat("&cYou died! You can join again 48 hours after your death!"));
-		/*
-		 * p.spigot().respawn(); if (!deathWorldService.movePlayerToHell(p)) {
-		 * plugin.log(messageService.getMessage(messageService.noDeathWorld)); }
-		 */
+		Date expires = new Date(System.currentTimeMillis() + 1000 * 3600 * hours);
+		String name = e.getEntity().getName();
+		Bukkit.getBanList(BanList.Type.NAME).addBan(name, "Died in Hardcore", expires, null);
+		plugin.log(String.format("Banned %s on death.", name));
 	}
 }
